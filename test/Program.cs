64535d22de67e02009f368b1c4f1db3ba8f2f6a3
Miguel Yax <mig_dj@hotmail.com>
2019-08-18 16:08:33 -0600

    First commit

---
+++ test/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine("Please enter a numeric argument.");
+                System.Console.WriteLine("Usage: Factorial <num>");
+                return 1;
+            }
+
+            // Try to convert the input arguments to numbers. This will throw
+            // an exception if the argument is not a number.
+            // num = int.Parse(args[0]);
+            int num;
+            bool test = int.TryParse(args[0], out num);
+            if (test == false)
+            {
+                System.Console.WriteLine("Please enter a numeric argument.");
+                System.Console.WriteLine("Usage: Factorial <num>");
+                return 1;
+            }
+
+            // Calculate factorial.
+            long result = Factorial(num);
+
+            // Print result.
+            if (result == -1)
+                System.Console.WriteLine("Input must be >= 0 and <= 20.");
+            else
+                System.Console.WriteLine("The Factorial of {0} is {1}.", num, result);
+
+            return 0;
+
+        }
+        public static long Factorial(int n)
+        {
+            // Test for invalid input
+            if ((n < 0) || (n > 20))
+            {
+                return -1;
+            }
+
+            // Calculate the factorial iteratively rather than recursively:
+            long tempResult = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                tempResult *= i;
+            }
+            return tempResult;
+        }
+    }
+}
